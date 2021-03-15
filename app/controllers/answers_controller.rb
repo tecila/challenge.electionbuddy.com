0314c0af18c7f858d3ddc9b284fecfdee1c86921
thamires tecila <t.tecila@gmail.com>
2021-03-15 10:03:34 -0300

    Add user params to answers and questions controllers to create audit

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -77,6 +77,6 @@ class AnswersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def answer_params
-    params.require(:answer).permit(:name)
+    params.require(:answer).permit(:name).merge(user: current_user)
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -77,6 +77,6 @@ class QuestionsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def question_params
-    params.require(:question).permit(:name)
+    params.require(:question).permit(:name).merge(user: current_user)
   end
 end

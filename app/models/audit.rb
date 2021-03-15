f40f4e57bc15128e8a76d8895e279ba042e05a6c
thamires tecila <t.tecila@gmail.com>
2021-03-15 09:57:50 -0300

    Add audit validation

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -4,5 +4,5 @@ class Audit < ApplicationRecord
   belongs_to :user
   belongs_to :auditable, polymorphic: true
 
-  validates :user, :auditable_id, :auditable_type, :audit_changes, presence: true
+  validates :user, :auditable_id, :auditable_type, :modifications, presence: true
 end
